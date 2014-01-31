9d046c2dcf5da4f7014012a8329fa09087cd9384
Szebenyi Balint <szebenyib@saturn>
2014-01-31 20:34:22 +0100

    Update static url path so as to serve static content from proper loaction.

--- bookshelf/settings.py
+++ bookshelf/settings.py
@@ -79,7 +79,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/bookshelf/static/'
 
 # Have local settings loaded when not in production
 try: 
